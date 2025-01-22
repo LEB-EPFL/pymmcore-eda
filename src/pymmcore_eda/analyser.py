ab83c14aacd4f07a14474bb5963e289c28e8d515
gtortarolo <giorgio.tortarolo@iit.it>
2025-01-22 09:51:34 +0100

    Added saving of the network output. Note: need to change the main to pass the writer object to the analyser.

--- src/pymmcore_eda/analyser.py
+++ src/pymmcore_eda/analyser.py
@@ -11,6 +11,7 @@ from threading import Thread
 from smart_scan.helpers.function_helpers import normalize_tilewise_vectorized
 
 from src.pymmcore_eda._logger import logger
+from src.pymmcore_eda.writer import AdaptiveWriter
 
 if TYPE_CHECKING:
     import numpy as np
@@ -18,7 +19,9 @@ if TYPE_CHECKING:
     from useq import MDAEvent
 
 class AnalyserSettings:
-    model_path: str = "//sb-nas1.rcp.epfl.ch/leb/Scientific_projects/deep_events_WS/data/original_data/training_data/20240826_1134_brightfield_cos7_n3_f1/20240826_1134_0_model.h5"
+    model_path: str = "/Volumes/LEB/Scientific_projects/deep_events_WS/data/original_data/training_data/20240820_1508_brightfield_cos7_n3_f2/20240820_1508_0_model.h5"
+    #model_path: str = "//sb-nas1.rcp.epfl.ch/leb/Scientific_projects/deep_events_WS/data/original_data/training_data/20240826_1134_brightfield_cos7_n3_f1/20240826_1134_0_model.h5"
+    
 
 @tf.keras.utils.register_keras_serializable(package='deep_events', name='wmse_loss')
 class WMSELoss(tf.keras.losses.Loss):
@@ -53,22 +56,25 @@ tf.keras.utils.get_custom_objects().update({'WBCELoss': WBCELoss})
 class Analyser:
     """Analyse the image and produce an event score map for the interpreter."""
 
-    def __init__(self, hub: EventHub):
+    def __init__(self, hub: EventHub, writer: AdaptiveWriter):
         settings = AnalyserSettings()
         self.hub = hub
         self.hub.frameReady.connect(self._analyse)
-        self.model = keras.models.load_model(settings.model_path)
+        self.model = keras.models.load_model(settings.model_path, compile = False)
         print("Model loaded")
+        
+        # Needed to write the network output on another channel
+        self.writer = writer
+
         self.images = np.zeros((3, 2048, 2048))
+        self.dummy_data = imread(Path("/Volumes/LEB/Scientific_projects/smart_spatial_KG_WS_GT/sandbox/dummy-data/stk_0010_FOV_1_MMStack_Default.ome.tif"))
+        #self.dummy_data = imread(Path("C:/Users/glinka/Desktop/stk_0010_FOV_1_MMStack_Default.ome.tif"))
 
-        self.dummy_data = imread(Path("C:/Users/glinka/Desktop/stk_0010_FOV_1_MMStack_Default.ome.tif"))
-
-        # Test: to load cuDNN version 8100
+        # Perform an initial prediction (takes more time than all the following ones)
         img = self.dummy_data[0:3]
         input = img.swapaxes(0,2)
         input = np.expand_dims(input, 0)
         self.model.predict(input)
-        # Test: to load cuDNN version 8100
 
 
     def _analyse(self, img: np.ndarray, event: MDAEvent, metadata: dict):
@@ -88,7 +94,7 @@ class Analyser:
         logger.info('Fake data going in')
         self.img = self.dummy_data[self.event.index.get("t", 0)]
         self.images[-1] = self.img
-        
+
         def predict():
             if self.event.index.get("t", 0) < 2:
                 return
@@ -97,9 +103,26 @@ class Analyser:
                 input = self.images.swapaxes(0,2)
                 input = np.expand_dims(input, 0)
                 output = self.model.predict(input)
-                # output = np.ones((1, 2048, 2048, 1))
                 output = output[0, :, :, 0]
-                #print('MAX', np.max(output))
+
+                # Write the network output output
+                # Generate a fake event and modify its channel
+                fake_event = self.event.model_copy()
+                custom_channel = 2
+                fake_event.index["c"] = custom_channel
+                
+                # Call writer.frameReady() to store the network output
+                output_save = np.array(output*1e4)
+                output_save = output_save.astype('uint16')
+                self.writer.frameReady(frame = output_save, event=fake_event, meta={})
+
+                # # Determine the correct index for the new channel
+                # # (e.g., this could be based on time, position, etc.)
+                # channel_index = 1  # Adjust this index based on your setup
+                # # Write the frame to the store
+                # store[channel_index].write(output).result()
+
+
             self.images[:-1] = self.images[1:]
             logger.info("Analyser")
             self.hub.new_analysis.emit(output, event, metadata)
