6ac73544a431b597d8dae7491d842967a14b82ce
Kasia Glinka <kasiaaa.glinka@gmail.com>
2025-01-28 10:38:30 +0100

    Creating fake_event with MDAEvent(). Cropping the input to the model. Adding multiple fake prediction to avoid initial slow-downs.

--- src/pymmcore_eda/analyser.py
+++ src/pymmcore_eda/analyser.py
@@ -13,14 +13,19 @@ from smart_scan.helpers.function_helpers import normalize_tilewise_vectorized
 
 from src.pymmcore_eda._logger import logger
 from src.pymmcore_eda.writer import AdaptiveWriter
+from useq import MDAEvent
 
 if TYPE_CHECKING:
     import numpy as np
     from event_hub import EventHub
-    from useq import MDAEvent
+    
 
 class AnalyserSettings:
-    model_path: str = "//sb-nas1.rcp.epfl.ch/leb/Scientific_projects/deep_events_WS/data/original_data/training_data/20240224_0205_brightfield_cos7_n5_f1/20240224_0208_model.h5"
+    # model_path: str = "//sb-nas1.rcp.epfl.ch/leb/Scientific_projects/deep_events_WS/data/original_data/training_data/20250109_1956_brightfield_cos7_t0.2_f1_sFalse_mito_events_n753_sFalse/20250109_1956_1_model.h5"
+    
+    # LSTM model
+    model_path: str = "Z:/Scientific_projects/deep_events_WS/data/original_data/training_data/20250106_1954_brightfield_cos7_t0.2_f1_sFalse_mito_events_n753_sFalse/20250106_1954_0_model.h5"
+
 
 @tf.keras.utils.register_keras_serializable(package='deep_events', name='wmse_loss')
 class WMSELoss(tf.keras.losses.Loss):
@@ -69,17 +74,25 @@ class Analyser:
         # self.dummy_data = imread(Path("C:/Users/glinka/Desktop/stk_0010_FOV_1_MMStack_Default.ome.tif"))
         # img = self.dummy_data[0:3]
 
-        # Perform a first fake prediction - takes a long time 
+        # Perform a few first fake predictions - take a long time 
         img = self.images
         input = img.swapaxes(0,2)
         input = np.expand_dims(input, 0)
-        self.model.predict(input)
+
+        # Crop all the images to haste prediction
+        input_cropped = input[:, 512:1536, 512:1536, :]
+        self.model.predict(input_cropped)
+        self.model.predict(input_cropped)
+        self.model.predict(input_cropped)
+        self.model.predict(input_cropped)
+
+
 
 
     def _analyse(self, img: np.ndarray, event: MDAEvent, metadata: dict):
         
         # tile-wise normalisation of the image
-        tile_size = 128
+        tile_size = 256
         try:
             self.img = normalize_tilewise_vectorized(arr=img, tile_size=tile_size)
         except AssertionError as e:
@@ -103,25 +116,49 @@ class Analyser:
                 logger.info('PREDICTING')
                 input = self.images.swapaxes(0,2)
                 input = np.expand_dims(input, 0)
-                output = self.model.predict(input)
-                output = output[0, :, :, 0]
 
+                # Crop all the images to haste prediction
+                input_cropped = input[:, 512:1536, 512:1536, :]
+                # input_cropped = input[:, 768:1280, 768:1280, :]
+
+                output_cropped = self.model.predict(input_cropped)
+                output_cropped = output_cropped[0, :, :, 0]
+                
+                output = np.zeros((2048, 2048))
+                output[512:1536, 512:1536] = output_cropped
+                # output[768:1280, 768:1280] = output_cropped
+
+
+                # Original prediction
+                # output = self.model.predict(input)
+                # output = output[0, :, :, 0]
+
+                print(f'Maximum value of the model output: {np.max(output)}\n')
+
+                self.images[:-1] = self.images[1:]
+                logger.info("Analyser")
+                self.hub.new_analysis.emit(output, event, metadata)
+                
                 # Write the network output output
                 # Generate a fake event and modify its channel
-                fake_event = self.event.model_copy()
+                # fake_event = self.event.model_copy()
+
+                # Create a new event
+                t_index = self.event.index.get("t", 0)
                 custom_channel = 2
-                fake_event.index["c"] = custom_channel
+
+                fake_event = MDAEvent(channel=self.event.channel, index={"t": t_index, "c": custom_channel}, min_start_time=0)
+
+                # fake_event.index["c"] = custom_channel
                 
                 # Call writer.frameReady() to store the network output
                 output_save = np.array(output*1e4)
+                output_save = np.transpose(output_save)
                 output_save = output_save.astype('uint16')
 
                 meta = FrameMetaV1(fake_event)
                 self.writer.frameReady(frame = output_save, event=fake_event, meta=meta)
 
-            self.images[:-1] = self.images[1:]
-            logger.info("Analyser")
-            self.hub.new_analysis.emit(output, event, metadata)
 
         predict_thread = Thread(target=predict)
         predict_thread.start()
