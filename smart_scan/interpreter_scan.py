2a6df2aa6b2943c80e1a806a6a0c12179936f30a
gtortarolo <giorgio.tortarolo@iit.it>
2025-02-01 12:54:11 +0100

    interpreter.py hosts the Interpreter_widefield class. Added interpreter_scan.py for the Interpreter_scan class.

--- smart_scan/interpreter_scan.py
+++ smart_scan/interpreter_scan.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 import numpy as np
-from PIL import Image
 
 if TYPE_CHECKING:
     import numpy as np
@@ -14,7 +13,7 @@ class InterpreterSettings:
     threshold: float = 0.9
     square_semi_size: int = 60 #px
 
-class Interpreter_widefield:
+class Interpreter_scan:
     """Get event score and produce a binary image that informs the actuator."""
 
     def __init__(self, hub: EventHub):
@@ -22,10 +21,14 @@ class Interpreter_widefield:
         self.hub.new_analysis.connect(self._interpret)
 
     def _interpret(self, net_out: np.ndarray, event: MDAEvent, metadata: dict):
-        logger.info("Interpreter")
+        logger.info("Interpreter for smart scan")
         mask = net_out > InterpreterSettings.threshold
+        coordinates = np.argwhere(mask)
+        
+        for coord in coordinates:
+            x, y = coord[0], coord[1]
+            mask[x - InterpreterSettings.square_semi_size : x + InterpreterSettings.square_semi_size, y - InterpreterSettings.square_semi_size : y + InterpreterSettings.square_semi_size] = 1
         
         # Emit the interpretation result only if not empty
         if np.sum(mask) != 0:
             self.hub.new_interpretation.emit(mask, event, metadata)
-    
