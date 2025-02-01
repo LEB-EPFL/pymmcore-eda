9c05c9b2159a680589f9fd9929ae9e24f6292073
gtortarolo <giorgio.tortarolo@iit.it>
2025-02-01 12:58:26 +0100

    The class AdaptiveWriter handles smart scan and smart widefield events

--- src/pymmcore_eda/writer.py
+++ src/pymmcore_eda/writer.py
@@ -63,11 +63,15 @@ class AdaptiveWriter(TensorStoreHandler):
             self, frame: np.ndarray, event: useq.MDAEvent, meta: FrameMetaV1
         ) -> None:
         if event.index.get("c", 0) == 1:
-            new_metadata = event.metadata.copy()
-            new_metadata['0'][0] = json.dumps({'0': event.metadata['0'][0].tolist()})
-            event = event.replace(metadata = new_metadata)
-            # event = event.replace(metadata = {})
-            meta['mda_event'] = meta['mda_event'].replace(metadata = {})
+            
+            # convert metadata to json for writing, only if event is smart scan 
+            try:
+                new_metadata = event.metadata.copy()
+                new_metadata['0'][0] = json.dumps({'0': event.metadata['0'][0].tolist()})
+                event = event.replace(metadata = new_metadata)
+                meta['mda_event'] = meta['mda_event'].replace(metadata = {})
+            except:
+                pass
         super().frameReady(frame, event, meta)
 
     def get_shape_chunks_labels(self, frame_shape, seq):
